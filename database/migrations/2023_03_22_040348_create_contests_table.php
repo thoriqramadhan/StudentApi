9148eb69955cb616258826ef5e34d2561918c257
Thoriq R <respect.thoriq@gmail.com>
2023-03-22 11:06:56 +0700

    create table

--- database/migrations/2023_03_22_040348_create_contests_table.php
+++ database/migrations/2023_03_22_040348_create_contests_table.php
@@ -13,14 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('contests', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('firstname');
-            $table->string('lastname')->nullable();
+            $table->string('title');
+            $table->text('level');
+            $table->unsignedBigInteger('author');
             $table->timestamps();
+
+            $table->softDeletes();
+            $table->foreign('author')->references('id')->on('users');
         });
     }
 
@@ -31,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('contests');
     }
 };
--- database/migrations/2023_03_22_040415_create_comments_table.php
+++ database/migrations/2023_03_22_040415_create_comments_table.php
@@ -13,14 +13,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('firstname');
-            $table->string('lastname')->nullable();
+            $table->unsignedBigInteger('contest_id');
+            $table->unsignedBigInteger('user_id');
+            $table->text('comment_content');
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('contest_id')->references('id')->on('contests');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -31,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 };
