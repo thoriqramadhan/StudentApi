362704eb13f68bc8ae9b67d27dea39a4254d77d9
Thoriq R <respect.thoriq@gmail.com>
2023-03-22 11:25:34 +0700

    get contest content

--- app/Http/Controllers/ContestController.php
+++ app/Http/Controllers/ContestController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ContestResource;
+use App\Models\Contest;
 use Illuminate\Http\Request;
 
 class ContestController extends Controller
 {
-    //
+    public function index(){
+        $contest = Contest::all();
+        return ContestResource::collection($contest);
+    }
+
+    public function show($id){
+        $contest = Contest::findOrFail($id);
+
+        return new PostDetailResour
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/contest',[]);
+Route::get('/contest',[ContestController::class, 'index']);
